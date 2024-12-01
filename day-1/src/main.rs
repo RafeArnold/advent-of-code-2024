748ce5bbd15cf8011d7a26ab3677007b5e238ef5
rafearnold <rafe.arnold@gmail.com>
2024-12-01 21:48:42 +0000

    day 1 part 2.

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,16 +1,26 @@
 fn main() {
     const INPUT: &str = include_str!("../input.txt");
     println!("{}", run_1(INPUT));
+    println!("{}", run_2(INPUT));
 }
 
-fn run_1(input: &str) -> u32 {
-    let (mut col1, mut col2): (Vec<_>, Vec<_>) = input.lines().map(parse_line).unzip();
+fn run_1(input: &str) -> usize {
+    let (mut col1, mut col2) = parse_lines(input);
     quick_sort(&mut col1);
     quick_sort(&mut col2);
     col1.iter().zip(col2).map(|(a, b)| a.abs_diff(b)).sum()
 }
 
-fn parse_line(line: &str) -> (u32, u32) {
+fn run_2(input: &str) -> usize {
+    let (col1, col2) = parse_lines(input);
+    col1.iter().map(|a| col2.iter().filter(|b| a == *b).count() * a).sum()
+}
+
+fn parse_lines(input: &str) -> (Vec<usize>, Vec<usize>) {
+    input.lines().map(parse_line).unzip()
+}
+
+fn parse_line(line: &str) -> (usize, usize) {
     let pos = line.chars().position(|c| c == ' ').unwrap();
     (
         line[0..pos].parse().unwrap(),
@@ -18,7 +28,7 @@ fn parse_line(line: &str) -> (u32, u32) {
     )
 }
 
-fn quick_sort(slice: &mut [u32]) {
+fn quick_sort(slice: &mut [usize]) {
     if slice.len() <= 1 {
         return;
     }
@@ -27,7 +37,7 @@ fn quick_sort(slice: &mut [u32]) {
     quick_sort(&mut slice[p + 1..]);
 }
 
-fn partition(slice: &mut [u32]) -> usize {
+fn partition(slice: &mut [usize]) -> usize {
     let pivot = slice[slice.len() - 1];
     let mut i = 0;
     for j in 0..slice.len() - 1 {
@@ -60,5 +70,16 @@ mod tests {
 3   9
 3   3";
         assert_eq!(run_1(INPUT), 11);
+    }
+
+    #[test]
+    fn challenge_2() {
+        const INPUT: &str = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+        assert_eq!(run_2(INPUT), 31);
     }
 }
