bf32c5a74dbbed4512c66f88bade309a6a4121e8
rafearnold <rafe.arnold@gmail.com>
2024-12-04 09:38:26 +0000

    strengthen day 3 part 1 solution.

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,3 +1,4 @@
+use std::iter::Peekable;
 use std::str::Chars;
 
 fn main() {
@@ -6,69 +7,65 @@ fn main() {
 }
 
 fn run_1(input: &str) -> usize {
-    let mut chars = input.chars();
+    let mut chars = input.chars().peekable();
     let mut sum = 0;
     loop {
-        match read_mul(&mut chars) {
-            Ok((first, second)) => sum += first * second,
-            Err(true) => return sum,
-            Err(false) => continue,
+        let c = chars.peek();
+        match c {
+            Some('m') => match read_mul(&mut chars) {
+                Some((first, second)) => sum += first * second,
+                None => continue,
+            },
+            Some(_) => {
+                chars.next();
+            }
+            None => return sum,
         }
     }
 }
 
-macro_rules! expect_char_or {
-    ($iter:ident, $pattern:pat, $or:ident $(,)?) => {{
-        let c = next_char($iter)?;
-        if (matches!(c, $pattern)) {
-            Ok(Some(c))
-        } else if c == $or {
-            Ok(None)
-        } else {
-            Err(false)
-        }
-    }};
-}
-
-fn read_mul(chars: &mut Chars) -> Result<(usize, usize), bool> {
+fn read_mul(chars: &mut Peekable<Chars>) -> Option<(usize, usize)> {
     expect_char(chars, 'm')?;
     expect_char(chars, 'u')?;
     expect_char(chars, 'l')?;
     expect_char(chars, '(')?;
-    let first = expect_num(chars, ',')?;
-    let second = expect_num(chars, ')')?;
-    Ok((first, second))
+    let first = expect_num(chars)?;
+    expect_char(chars, ',')?;
+    let second = expect_num(chars)?;
+    expect_char(chars, ')')?;
+    Some((first, second))
 }
 
-fn expect_num(chars: &mut Chars, suffix: char) -> Result<usize, bool> {
+fn expect_num(chars: &mut Peekable<Chars>) -> Option<usize> {
     let mut digits = Vec::with_capacity(3);
     for _ in 0..3 {
-        let c = expect_char_or!(chars, '0'..='9', suffix);
-        if let Some(c) = c? {
+        let c = *chars.peek()?;
+        if matches!(c, '0'..='9') {
+            chars.next();
             digits.push(c);
         } else {
-            return Ok(parse_num(digits));
+            break;
         }
     }
-    expect_char(chars, suffix)?;
-    Ok(parse_num(digits))
+    if digits.is_empty() {
+        None
+    } else {
+        Some(parse_num(digits))
+    }
 }
 
 fn parse_num(digits: Vec<char>) -> usize {
     digits.into_iter().collect::<String>().parse().unwrap()
 }
 
-fn expect_char(chars: &mut Chars, expected: char) -> Result<char, bool> {
-    let c = next_char(chars)?;
+fn expect_char(chars: &mut Peekable<Chars>, expected: char) -> Option<char> {
+    let c = *chars.peek()?;
     if c == expected {
-        Ok(c)
+        chars.next();
+        Some(c)
     } else {
-        Err(false)
+        None
     }
-}
-
-fn next_char(chars: &mut Chars) -> Result<char, bool> {
-    chars.next().ok_or(true)
 }
 
 #[cfg(test)]
@@ -79,6 +76,27 @@ mod tests {
     fn challenge_1() {
         const INPUT: &str =
             "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        assert_eq!(run_1(INPUT), 161);
+    }
+
+    #[test]
+    fn valid_mul_intercepting_invalid_mul() {
+        const INPUT: &str =
+            "xmul(2,4)%&mul[3,7]!@^do_not_mul(5mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        assert_eq!(run_1(INPUT), 161);
+    }
+
+    #[test]
+    fn mul_with_no_digit() {
+        const INPUT: &str =
+            "xmul(2,4)%&mul[3,7]!@^do_not_mul(mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        assert_eq!(run_1(INPUT), 161);
+    }
+
+    #[test]
+    fn eof_mid_number() {
+        const INPUT: &str =
+            "xmul(2,4)%&mul[3,7]!@^do_not_mul(mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5)mul(8";
         assert_eq!(run_1(INPUT), 161);
     }
 }
