3d07ab07173e6413c4922822d6dcd92f6a95014e
rafearnold <rafe.arnold@gmail.com>
2024-12-07 18:29:18 +0100

    day 4 part 2.

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     const INPUT: &str = include_str!("../input.txt");
     println!("{}", run_1(INPUT));
+    println!("{}", run_2(INPUT));
 }
 
 const XMAS: [u8; 4] = [b'X', b'M', b'A', b'S'];
@@ -44,6 +45,44 @@ fn run_1(input: &str) -> usize {
     sum
 }
 
+fn run_2(input: &str) -> usize {
+    let lines = input.lines().map(|str| str.as_bytes()).collect::<Vec<_>>();
+    let mut sum = 0;
+    for (row, &line) in lines.iter().enumerate() {
+        for (col, &byte) in line.iter().enumerate() {
+            sum += is_x_mas(&lines, row, col, byte).is_some() as usize;
+        }
+    }
+    sum
+}
+
+fn is_x_mas(lines: &[&[u8]], row: usize, col: usize, byte: u8) -> Option<()> {
+    if byte != b'A' {
+        return None;
+    }
+    {
+        let point1 = (row.checked_sub(1)?, col.checked_sub(1)?);
+        let point2 = (row + 1, col + 1);
+        if point2.0 >= lines.len() || point2.1 >= lines[0].len() || !is_mas(lines, point1, point2) {
+            return None;
+        }
+    }
+    {
+        let point1 = (row.checked_sub(1)?, col + 1);
+        let point2 = (row + 1, col.checked_sub(1)?);
+        if point2.0 >= lines.len() || point1.1 >= lines[0].len() || !is_mas(lines, point1, point2) {
+            return None;
+        }
+    }
+    Some(())
+}
+
+fn is_mas(lines: &[&[u8]], point1: (usize, usize), point2: (usize, usize)) -> bool {
+    let byte1 = lines[point1.0][point1.1];
+    let byte2 = lines[point2.0][point2.1];
+    (byte1 == b'M' && byte2 == b'S') || (byte1 == b'S' && byte2 == b'M')
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -61,5 +100,20 @@ SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX";
         assert_eq!(run_1(INPUT), 18);
+    }
+
+    #[test]
+    fn challenge_2() {
+        const INPUT: &str = "MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX";
+        assert_eq!(run_2(INPUT), 9);
     }
 }
