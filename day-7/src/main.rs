294fbd35b29edd30f7e3e4a4f99f73f5882d553e
rafearnold <rafe.arnold@gmail.com>
2024-12-07 22:21:08 +0100

    day 7 part 2.

--- day-7/src/main.rs
+++ day-7/src/main.rs
@@ -1,24 +1,43 @@
 fn main() {
     const INPUT: &str = include_str!("../input.txt");
     println!("{}", run_1(INPUT));
+    println!("{}", run_2(INPUT));
 }
 
 fn run_1(input: &str) -> u64 {
+    run(input, false)
+}
+
+fn run_2(input: &str) -> u64 {
+    run(input, true)
+}
+
+fn run(input: &str, can_concat: bool) -> u64 {
     parse_input(input)
         .into_iter()
-        .filter(|(test_value, operands)| could_be_true(*test_value, operands))
+        .filter(|(test_value, operands)| could_be_true(*test_value, operands, can_concat))
         .map(|equation| equation.0)
         .sum()
 }
 
-fn could_be_true(test_value: u64, operands: &[u64]) -> bool {
+fn could_be_true(test_value: u64, operands: &[u64], can_concat: bool) -> bool {
     if operands.len() == 1 {
         return test_value == operands[0];
     }
     let (rest, tail) = operands.split_at(operands.len() - 1);
     let tail = tail[0];
-    test_value > tail && could_be_true(test_value - tail, rest)
-        || test_value % tail == 0 && could_be_true(test_value / tail, rest)
+    can_concat && could_be_true_concat(test_value, tail, rest)
+        || test_value % tail == 0 && could_be_true(test_value / tail, rest, can_concat)
+        || test_value > tail && could_be_true(test_value - tail, rest, can_concat)
+}
+
+fn could_be_true_concat(test_value: u64, tail: u64, rest: &[u64]) -> bool {
+    if test_value <= tail {
+        return false;
+    }
+    let power_of_10 = 10u64.pow(tail.ilog10() + 1);
+    (test_value - tail) % power_of_10 == 0
+        && could_be_true((test_value - tail) / power_of_10, rest, true)
 }
 
 fn parse_input(input: &str) -> Vec<(u64, Vec<u64>)> {
@@ -48,5 +67,10 @@ mod tests {
     #[test]
     fn challenge_1() {
         assert_eq!(run_1(INPUT), 3749);
+    }
+
+    #[test]
+    fn challenge_2() {
+        assert_eq!(run_2(INPUT), 11387);
     }
 }
