a9576648179d3de448b244b4e7b165049a56a759
rafearnold <rafe.arnold@gmail.com>
2024-12-09 13:36:34 +0100

    day 8 part 2.

--- day-8/src/main.rs
+++ day-8/src/main.rs
@@ -3,42 +3,76 @@ use std::collections::{HashMap, HashSet};
 fn main() {
     const INPUT: &str = include_str!("../input.txt");
     println!("{}", run_1(INPUT));
+    println!("{}", run_2(INPUT));
 }
 
 fn run_1(input: &str) -> usize {
-    let size = (input.lines().count(), input.lines().next().unwrap().len());
-    let mut antennas: HashMap<u8, Vec<(usize, usize)>> = HashMap::new();
+    run(input, false)
+}
+
+fn run_2(input: &str) -> usize {
+    run(input, true)
+}
+
+fn run(input: &str, count_all: bool) -> usize {
+    let size = (
+        input.lines().count() as isize,
+        input.lines().next().unwrap().len() as isize,
+    );
+    let mut antennas: HashMap<u8, Vec<(isize, isize)>> = HashMap::new();
     for (row, line) in input.lines().enumerate() {
         for (col, byte) in line.bytes().enumerate() {
             if byte != b'.' {
-                antennas.entry(byte).or_default().push((row, col));
+                antennas
+                    .entry(byte)
+                    .or_default()
+                    .push((row as isize, col as isize));
             }
         }
     }
     antennas
         .into_values()
-        .map(get_anti_nodes)
-        .flatten()
+        .flat_map(|antennas| get_anti_nodes(antennas, size, count_all))
         .filter(|&(x, y)| x < size.1 && y < size.0)
         .collect::<HashSet<_>>()
         .len()
 }
 
-fn get_anti_nodes(antennas: Vec<(usize, usize)>) -> HashSet<(usize, usize)> {
+fn get_anti_nodes(
+    antennas: Vec<(isize, isize)>,
+    boundary: (isize, isize),
+    count_all: bool,
+) -> HashSet<(isize, isize)> {
     let mut anti_nodes = HashSet::new();
     for (i, &(row_a, col_a)) in antennas.iter().enumerate() {
         for &(row_b, col_b) in antennas.iter().skip(i + 1) {
-            // We're assuming row_b >= row_a
-            let (y, x) = (row_b - row_a, col_b as isize - col_a as isize);
-            if col_b as isize >= -x {
-                anti_nodes.insert((row_b + y, (col_b as isize + x) as usize));
-            }
-            if row_a >= y && col_a as isize >= x {
-                anti_nodes.insert((row_a - y, (col_a as isize - x) as usize));
+            let (y, x) = (row_b - row_a, col_b - col_a);
+            if count_all {
+                scan(boundary, &mut anti_nodes, (-y, -x), (row_a, col_a));
+                scan(boundary, &mut anti_nodes, (y, x), (row_b, col_b));
+            } else {
+                if col_b >= -x {
+                    anti_nodes.insert((row_b + y, col_b + x));
+                }
+                if row_a >= y && col_a >= x {
+                    anti_nodes.insert((row_a - y, col_a - x));
+                }
             }
         }
     }
     anti_nodes
+}
+
+fn scan(
+    boundary: (isize, isize),
+    anti_nodes: &mut HashSet<(isize, isize)>,
+    to_move: (isize, isize),
+    mut pos: (isize, isize),
+) {
+    while pos.0 >= 0 && pos.1 >= 0 && pos.0 < boundary.0 && pos.1 < boundary.1 {
+        anti_nodes.insert((pos.0, pos.1));
+        pos = (pos.0 + to_move.0, pos.1 + to_move.1);
+    }
 }
 
 #[cfg(test)]
@@ -61,5 +95,10 @@ mod tests {
     #[test]
     fn challenge_1() {
         assert_eq!(run_1(INPUT), 14);
+    }
+
+    #[test]
+    fn challenge_2() {
+        assert_eq!(run_2(INPUT), 34);
     }
 }
