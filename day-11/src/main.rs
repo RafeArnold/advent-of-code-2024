ab4d06827e740ed5ffdb7aade8bba96976c90055
rafearnold <rafe.arnold@gmail.com>
2024-12-11 20:39:21 +0000

    day 11 part 2.

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -1,32 +1,47 @@
+use std::collections::HashMap;
+
 fn main() {
     const INPUT: &str = include_str!("../input.txt");
     println!("{}", run_1(INPUT));
+    println!("{}", run_2(INPUT));
 }
 
 fn run_1(input: &str) -> usize {
-    let mut stones = input
-        .split_whitespace()
-        .map(|x| x.parse::<usize>().unwrap())
-        .collect::<Vec<_>>();
-    for _ in 0..25 {
-        for i in (0..stones.len()).rev() {
-            let stone = stones[i];
-            if stone == 0 {
-                stones[i] = 1;
-                continue;
-            }
-            let log10 = stone.ilog10();
-            if log10 % 2 == 1 {
-                let pow = 10usize.pow(log10 / 2 + 1);
-                let upper = stone / pow;
-                stones[i] = upper;
-                stones.insert(i + 1, stone - upper * pow);
-            } else {
-                stones[i] *= 2024;
+    run(input, 25)
+}
+
+fn run_2(input: &str) -> usize {
+    run(input, 75)
+}
+
+fn run(input: &str, iterations: u8) -> usize {
+    let mut stones = HashMap::new();
+    for stone in input.split_whitespace() {
+        *stones.entry(stone.parse::<usize>().unwrap()).or_default() += 1;
+    }
+    for _ in 0..iterations {
+        let mut new_stones = HashMap::new();
+        for (stone, count) in stones {
+            for new_stone in next_stones(stone) {
+                *new_stones.entry(new_stone).or_default() += count;
             }
         }
+        stones = new_stones;
     }
-    stones.len()
+    stones.values().sum()
+}
+
+fn next_stones(stone: usize) -> Vec<usize> {
+    if stone == 0 {
+        return vec![1];
+    }
+    let log10 = stone.ilog10();
+    if log10 % 2 == 1 {
+        let pow = 10usize.pow(log10 / 2 + 1);
+        vec![stone / pow, stone % pow]
+    } else {
+        vec![stone * 2024]
+    }
 }
 
 #[cfg(test)]
